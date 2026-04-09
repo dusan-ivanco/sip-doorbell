14c4e7eae548486f6d865f1ce13f6be236df2aa9
Dušan Ivančo <ivanco@supervisor.sk>
2026-04-09 18:00:13 +0200

    2026.04.09

---
+++ sip-doorbell.js
@@ -0,0 +1,482 @@
+import '/hacsfiles/sip-doorbell/jssip.js';
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type:'sip-doorbell',
+  name:'SIP Doorbell',
+  description:'The SIP Doorbell card allows you to integrate your intercom unit.'
+});
+
+class sipDoorbell extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode:'open'});
+    window.sipDoorbell = window.sipDoorbell || {};
+
+    this.version = '0.0.1';
+    this.startup = {
+      content:false,
+      control:false
+    };
+
+    (() => {
+      const child = new MutationObserver((m) => {
+        m.forEach((l) => {
+          l.addedNodes.forEach((e) => {
+            if(e.nodeName === 'HA-CARD') {
+              focus.observe(e);
+            }
+          });
+        });
+      });
+
+      const focus = new IntersectionObserver((i) => {
+        i.forEach((e) => {
+          if(e.isIntersecting) {
+            this.stretch();
+          }
+        });
+      }, {
+        root:this.shadowRoot.activeElement
+      });
+
+      child.observe(this.shadowRoot, {
+        attributes:false,
+        childList:true,
+        subtree:false
+      });
+
+      ['orientationchange', 'resize'].forEach((e) => {
+        window.addEventListener(e, () => this.stretch());
+      });
+    })();
+
+    window.addEventListener('beforeunload', () => {
+      if(window.sipDoorbell[this.config.worker]) {
+        window.sipDoorbell[this.config.worker].stop();
+      }
+    });
+  }
+
+  getCardSize() {
+    return 1;
+  }
+
+  setConfig(config) {
+    this.config = {
+      server:{
+        sip:config?.server?.sip ? config?.server?.sip : {},
+        opt:{
+          mediaConstraints:{
+            audio:true,
+            video:true
+          },
+          pcConfig:{
+            iceServers:config?.server?.ice ? config?.server?.ice : [],
+            bundlePolicy:'balanced',
+            rtcpMuxPolicy:'require',
+            iceTransportPolicy:'all',
+            iceCandidatePoolSize:0
+          }
+        }
+      },
+      access:config?.access ? config?.access : [],
+      return:config?.return ? config?.return : '',
+      camera:{
+        entity:config?.camera ? config?.camera : '',
+        player:''
+      }
+    };
+
+    crypto.subtle.digest('SHA-1', new TextEncoder().encode(JSON.stringify(this.config.server.sip))).then((b) => {
+      this.config.worker = Array.from(new Uint8Array(b)).map((d) => d.toString(16).padStart(2, '0')).join('');
+
+      if(this.startup.content === false) {
+        this.startup.content = true;
+        setTimeout(() => {
+          this.content();
+        }, 0);
+      }
+
+      if(this.startup.control === false) {
+        this.startup.control = true;
+        setTimeout(() => {
+          this.control();
+        }, 1000);
+      }
+    });
+
+    navigator.mediaDevices.getUserMedia({
+      audio:true,
+      video:true
+    }).then((e) => {
+      e.getTracks().forEach((t) => t.stop());
+    }).catch(() => {
+      throw new Error('SIP Doorbel require audio/video permission');
+    });
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+
+    if(this.config.camera.entity && this.config.camera.player) {
+      this.config.camera.player.hass = hass;
+      this.config.camera.player.stateObj = hass.states[this.config.camera.entity];
+    }
+  }
+
+  content() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        ha-icon {
+          margin: 30px;
+          padding: 15px;
+          cursor: pointer;
+
+          border-width: 2px;
+          border-radius: 50%;
+          border-style: solid;
+
+          color: white;
+          --mdc-icon-size: 30px;
+
+          transition: all 0.25s;
+          box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+        }
+
+        ha-icon:hover {
+          transform: scale(1.05);
+          box-shadow: 0 15px 25px rgba(0, 0, 0, 0.5);
+        }
+
+        ha-icon:active {
+          transform: scale(0.95);
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+        }
+
+        #basis {
+          min-width: 320px;
+          min-height: 240px;
+
+          width: 100%;
+          height: 100%;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          background: initial;
+          transition: background 0.5s ease;
+        }
+
+        #arena {
+          width: 0;
+          height: 0;
+          position: relative;
+        }
+
+        #audio {
+          width: 0;
+          height: 0;
+          display: none;
+          object-fit: contain;
+        }
+
+        #video {
+          width: 100%;
+          height: 100%;
+          display: none;
+          object-fit: contain;
+        }
+
+        #scene {
+          width: 100%;
+          height: 100%;
+          display: block;
+          object-fit: contain;
+        }
+
+        #panel {
+          left: 0;
+          right: 0;
+          bottom: 0;
+          position: absolute;
+        }
+
+        #panel span {
+          display: flex;
+          align-items: center;
+          justify-content: space-evenly;
+        }
+
+        [data-key="pickup"] {
+          display: none;
+          background: green;
+        }
+
+        [data-key="hangup"] {
+          display: none;
+          background: red;
+        }
+
+        #basis.loading {
+          background: black;
+        }
+
+        #basis:not(.loading) #hello {
+          display: none;
+        }
+
+        #hello {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 50px;
+          height: 50px;
+          z-index: 10;
+
+          border: 5px solid #333333;
+          border-top: 5px solid #ffffff;
+          border-radius: 50%;
+
+          transform: translate(-50%, -50%);
+          animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+          0% { transform: translate(-50%, -50%) rotate(0deg); }
+          100% { transform: translate(-50%, -50%) rotate(360deg); }
+        }
+
+        ${this.config.access.map((d, i) => `[data-key="C${i}"] {display: none; background: green;}`).join('')}
+      </style>
+
+      <ha-card id="basis" class="loading">
+        <div id="arena">
+          <div id="hello"></div>
+          <div id="scene"></div>
+          <audio id="audio" autoplay playsinline></audio>
+          <video id="video" autoplay playsinline></video>
+          <div id="panel">
+            <span>
+              ${this.config.access.map((d, i) => `<ha-icon data-key="C${i}" icon="${d.icon}"></ha-icon>`).join('')}
+              <ha-icon data-key="pickup" icon="mdi:phone"></ha-icon>
+              <ha-icon data-key="hangup" icon="mdi:phone-hangup"></ha-icon>
+            </span>
+          </div>
+        </div>
+      </ha-card>
+    `;
+  }
+
+  control() {
+    if(this.config.camera.entity) {
+      this.config.camera.player = Object.assign(document.createElement('ha-camera-stream'), {
+        hass:this._hass,
+        stateObj:this._hass.states[this.config.camera.entity]
+      });
+
+      this.config.camera.player.setAttribute('muted', '');
+      this.config.camera.player.style.setProperty('--video-max-height', '100vh');
+
+      this.element('#scene').replaceChildren(this.config.camera.player);
+    }
+
+    if(window.sipDoorbell[this.config.worker]) {
+      window.sipDoorbell[this.config.worker].stop();
+    }
+
+    window.sipDoorbell[this.config.worker] = new JsSIP.UA({
+      sockets:new JsSIP.WebSocketInterface(this.config.server.sip.url),
+      uri:'sip:' + this.config.server.sip.username,
+      password:this.config.server.sip.credential
+    });
+
+    if(window.sipDoorbell[this.config.worker]) {
+      this.element('#basis').classList.remove('loading');
+
+      window.sipDoorbell[this.config.worker].on('newRTCSession', (rtc) => {
+        const stream = (c) => {
+          c.ontrack = (e) => {
+            if(e.track.kind === 'audio') {
+              this.element('#audio').srcObject = e.streams[0];
+            }
+
+            if(e.track.kind === 'video') {
+              this.element('#video').srcObject = e.streams[0];
+            }
+          };
+        };
+
+        if(rtc.session.direction === 'incoming') {
+          console.info('%cIncoming call acquired:' + ' ' + rtc.session.remote_identity, 'color: blue;');
+          this.incoming_call_accepted = false;
+
+          this.element('[data-key="pickup"]').setAttribute('style', 'display: initial;');
+          this.element('[data-key="hangup"]').setAttribute('style', 'display: initial;');
+          this.config.access.forEach((d, i) => this.element('[data-key="C' + i + '"]').setAttribute('style', 'display: none;'));
+
+          rtc.session.on('accepted', () => {
+            if(this.incoming_call_accepted !== null) {
+              console.info('%cIncoming call accepted:' + ' ' + rtc.session.remote_identity, 'color: yellow;');
+              this.incoming_call_accepted = true;
+
+              this.element('#audio').setAttribute('style', 'display: none;');
+              this.element('#video').setAttribute('style', 'display: block;');
+              this.element('#scene').setAttribute('style', 'display: none;');
+
+              this.element('[data-key="pickup"]').setAttribute('style', 'display: none;');
+              this.element('[data-key="hangup"]').setAttribute('style', 'display: initial;');
+              this.config.access.forEach((d, i) => this.element('[data-key="C' + i + '"]').setAttribute('style', 'display: initial;'));
+            }
+          });
+
+          this.element('[data-key="pickup"]').addEventListener('click', () => {
+            rtc.session.answer(this.config.server.opt);
+          });
+
+          this.element('[data-key="hangup"]').addEventListener('click', () => {
+            if(this.incoming_call_accepted) {
+              rtc.session.terminate();
+            } else {
+              this.incoming_call_accepted = null;
+
+              rtc.session.on('accepted', () => {
+                rtc.session.terminate();
+              });
+
+              rtc.session.answer(this.config.server.opt);
+            }
+
+            if(this.config.return) {
+              this.dispatchEvent(new CustomEvent('hass-action', {
+                detail:{
+                  action:'tap',
+                  config:{
+                    tap_action:{
+                      action:'navigate',
+                      navigation_path:this.config.return
+                    }
+                  }
+                },
+                bubbles:true,
+                composed:true
+              }));
+            }
+          });
+        }
+
+        if(rtc.session.connection) {
+          stream(rtc.session.connection);
+        } else {
+          rtc.session.on('peerconnection', () => stream(rtc.session.connection));
+        }
+
+        rtc.session.on('ended', (e) => {
+          console.info('%cCall session ended:' + ' ' + e.cause, 'color: red;');
+          this.cleanup();
+        });
+
+        rtc.session.on('failed', (e) => {
+          console.info('%cCall session failed:' + ' ' + e.cause, 'color: red;');
+          this.cleanup();
+        });
+
+        this.config.access.forEach((d, i) => {
+          this.element('[data-key="C' + i + '"]').addEventListener('click', () => {
+            this.dispatchEvent(new CustomEvent('hass-action', {
+              detail:{
+                action:'tap',
+                config:d.click
+              },
+              bubbles:true,
+              composed:true
+            }));
+          });
+        });
+      });
+
+      window.sipDoorbell[this.config.worker].on('registered', () => {
+        console.info('%cSIP-DOORBELL ' + this.version + ' IS INSTALLED', 'color: green; font-weight: bold;');
+      });
+
+      window.sipDoorbell[this.config.worker].on('unregistered', () => {
+        console.info('%cSIP-DOORBELL ' + this.version + ' IS UNINSTALLED', 'color: yellow; font-weight: bold;');
+      });
+
+      window.sipDoorbell[this.config.worker].on('registrationFailed', () => {
+        console.info('%cSIP-DOORBELL ' + this.version + ' INSTALLATION FAILED', 'color: red; font-weight: bold;');
+      });
+
+      window.sipDoorbell[this.config.worker].start();
+    }
+
+    this.cleanup();
+  }
+
+  cleanup() {
+    this.factory('[data-key="pickup"]');
+    this.factory('[data-key="hangup"]');
+    this.config.access.forEach((d, i) => this.factory('[data-key="C' + i + '"]'));
+
+    this.element('#audio').removeAttribute('style');
+    this.element('#video').removeAttribute('style');
+    this.element('#scene').removeAttribute('style');
+  }
+
+  stretch() {
+    const b = this.element('#basis');
+    const a = this.element('#arena');
+
+    if(b && a) {
+      const d = b.getBoundingClientRect();
+
+      a.style.width = Math.max(0, (d.left > 0) ? Math.min(d.width, (window.innerWidth - d.left)) : Math.min(d.right, window.innerWidth)) + 'px';
+      a.style.height = Math.max(0, (d.top > 0) ? Math.min(d.height, (window.innerHeight - d.top)) : Math.min(d.bottom, window.innerHeight)) + 'px';
+    }
+  }
+
+  element(e) {
+    return this.shadowRoot.querySelector(e);
+  }
+
+  factory(e) {
+    const o = this.element(e);
+    const n = o.cloneNode(true);
+
+    n.removeAttribute('style');
+    o.parentNode.replaceChild(n, o);
+  }
+
+  static getStubConfig() {
+    return {
+      server:{
+        sip:{
+          url:'wss://sip.domain.tld/ws',
+          username:'001@sip.domain.tld',
+          credential:'secret'
+        },
+        ice:[{
+          urls:'stun:stun.domain.tld:3478'
+        }, {
+          urls:'turn:turn.domain.tld:3478',
+          username:'turn',
+          credential:'secret'
+        }]
+      },
+      access:[{
+        icon:'mdi:gate',
+        click:{
+          entity:'input_boolean.gate_lock',
+          tap_action:{
+            action:'toggle'
+          }
+        }
+      }],
+      return:'/lovelace/home',
+      camera:'camera.doorbell'
+    };
+  }
+}
+
+customElements.define('sip-doorbell', sipDoorbell);
